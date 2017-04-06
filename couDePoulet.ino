d08509e54f3b7fa85c6e208cf424ba7165bc173e
MajorLee95 <joel@vorobotics.com>
2017-04-06 21:31:30 +0200

    commit fonctionnelle

--- couDePoulet.ino
+++ couDePoulet.ino
@@ -4,7 +4,9 @@
 
 void setup()
 {
-Serial.println("on a presque fini");
+	Serial.begin(115200);
+	Serial.println("on a presque fini");
+	Serial.println("presque c'est PAS fini !'");
 }
 
 void loop()
